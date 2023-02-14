067b011e28e017c7473c379c94a2eda6f40b3c28
MyLlama <124775377+MyLlama@users.noreply.github.com>
2023-02-14 11:51:06 +0530

    Made changes to ask answers from spritual leaders

--- app.py
+++ app.py
@@ -11,15 +11,19 @@ co = cohere.Client(api_key)
 
 def generate_output(user_prompt):
   base_prompt = textwrap.dedent("""
-    Answer the below question as Lord Krishna would in Bhagavad Gita.
-    
+    Give six answers to the below question, 
+    Give the First answer as how jesus would have given the  answer to that question, 
+    Give the second answer as how prophet muhammad  would have given the  answer to that question, 
+    Give the third answer as how the lord krishna  would have given the answer to that question,
+    Give the forth answer as how osho would have given the  answer to that question,
+    Give the fifth answer as how Swami Vivekananda would have given the  answer to that question.
     Question:""")
 
   # Call the Cohere Generate endpoint
   response = co.generate( 
     model='command-xlarge-20221108', 
     prompt = base_prompt + " " + user_prompt + "\nAnswer: ",
-    max_tokens=100, 
+    max_tokens=300, 
     temperature=0.9,
     k=0, 
     p=0.7, 
@@ -34,11 +38,11 @@ def generate_output(user_prompt):
 
 # The front end code starts here
 
-st.title(" Ask Bhagavad Gita")
+st.title(" Ask Llama")
 
 form = st.form(key="user_settings")
 with form:
-  st.write("Enter a question [Example: How should i treat my enemies?] ")
+  st.write("Enter a question and Llama will give you answers from Bible, Quran and Gita")
   # User input - Question
   user_input = st.text_input("Question", key = "user_input")
 
@@ -50,7 +54,7 @@ with form:
       st.error("Question cannot be blank")
     else:
       my_bar = st.progress(0.05)
-      st.subheader("Answer:")
+      st.subheader("Answers from Bible, Quran and Gita:")
 
       for i in range(num_input):
           st.markdown("""---""")
@@ -59,4 +63,4 @@ with form:
           my_bar.progress((i+1)/num_input)
 
 st.write( '')
-st.markdown("Created by[Harish Garg](https://harishgarg.com) Get [Source Code](https://github.com/harish-garg/AskAIBhagavadGita)")
+st.markdown("Created by [LLAMA](https://www.myllama.co/), inspired by AskAIBhagvadGita")
